fa66cd46e0b270664380f34c4b67468f0ffff493
Michael-August <michaelfrancis135@gmail.com>
2022-07-01 20:39:43 +0100

    added rating

--- src/app/shared/services/resturants/resturants.service.ts
+++ src/app/shared/services/resturants/resturants.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BASE_URL } from 'src/environments/environment';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResturantsService {
 
+  private subject = new Subject()
+
   constructor(private http: HttpClient) { }
 
   getResturants() {
     return this.http.get(`${BASE_URL}/resturants`)
+  }
+
+  getResturantsWithParams(param: any) {
+    return this.http.get(`${BASE_URL}/resturants?name=${param.search_term}`)
+  }
+
+  sendSearchParams(params: any) {
+    this.subject.next(params)
+  }
+
+  receiveSearchParams() {
+    return this.subject.asObservable()
   }
 
 }
